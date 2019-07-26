a20de912f3078165e517408fa17c23aa37dc28cb
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-07-26 21:38:29 +0900

    各validationを設定

--- src/main/java/video/video/dto/MovieDto.java
+++ src/main/java/video/video/dto/MovieDto.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -14,13 +16,18 @@ public class MovieDto {
     private int id;
     @NotEmpty(message = "タイトルを入力してください")
     private String title;
+    @NotEmpty(message = "検索用タイトルを入力してください")
     private String searchTitle;
-    private int releaseYear;
+    @NotNull(message = "リリース年を入力してください")
+    private Integer releaseYear;
+    @NotEmpty(message = "言語を選択してください")
     private String originalLanguage;
-    private int showTimes;
+    @NotNull(message = "上映時間を入力してください")
+    private Integer showTimes;
     private Integer starringNum1;
     private Integer starringNum2;
     private Integer starringNum3;
     private Integer starringNum4;
+    @NotNull(message = "閲覧ステータスを選択してください")
     private boolean watchedFlg;
 }
