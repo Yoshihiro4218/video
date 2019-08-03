78fbc43bffe5f64a1fbef30a87174d0c451e7d83
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-08-03 13:03:49 +0900

    出演者が誰もいなかったときにヌルポで死ぬ問題の修正

--- src/main/java/video/video/service/MovieService.java
+++ src/main/java/video/video/service/MovieService.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 import video.video.dto.MovieDto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -71,7 +72,7 @@ public class MovieService {
 
     public List showActorDetailWithMovie(MovieDto movie) {
         String actorSql;
-        List actorList = null;
+        List actorList = new ArrayList();
         if(movie.getStarringNum4()!=null){
             actorSql = "SELECT actor.name FROM movie JOIN actor ON " +
                     "movie.starring_num1 = actor.id " +
